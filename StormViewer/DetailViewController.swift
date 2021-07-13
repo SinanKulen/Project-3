58c6aa8980edc87217a199ea1eb1ecd92ff87d4a
SinanKulen <sinan.kulen@gmail.com>
2021-07-13 13:19:37 +0300

    Project3

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Sinan Kulen on 2.07.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var selectedImage : String?
+    @IBOutlet var imageView: UIImageView!
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title =  "\(totalPictures) in  \(selectedPictureNumber)"
+        
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let imageName = selectedImage
+        let vc = UIActivityViewController(activityItems: [image,imageName!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true, completion: nil)
+        
+    }
+
+}
